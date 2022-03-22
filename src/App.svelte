6fc6cd9e15d2017aff5551a75f88ed9ecf7002f1
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-03-22 20:01:32 -0300

    aula-3

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,78 @@
 <script lang="ts">
-	import Titulo from './components/Titulo.svelte';
+  import BarraSuperior from "./components/BarraSuperior.svelte";
+  import Titulo from "./components/Titulo.svelte";
+  import type IUsuario from "./interfaces/IUsuario";
 
-  let valorInput = 'antonio';
+  let usuario: IUsuario | null = null;
+
+  let valorInput = "antonio";
 
   function aoSubmeter() {
-    console.log(valorInput);
+    usuario = {
+      avatar_url: "https://github.com/antonio-evaldo.png",
+      login: "antonio-evaldo",
+      nome: "Antônio Evaldo",
+      perfil_url: "https://github.com/antonio-evaldo",
+      repositorios_publicos: 50,
+      seguidores: 20,
+    };
   }
 </script>
 
 <div class="app">
-	<header>
-		<Titulo />
+  <header>
+    <Titulo />
 
     <div class="busca-usuario">
       <form on:submit|preventDefault={aoSubmeter}>
-        <input type="text" class="input" bind:value={valorInput}>
+        <input type="text" class="input" bind:value={valorInput} />
 
         <div class="botao-container">
           <button type="submit" class="botao">Buscar</button>
         </div>
       </form>
     </div>
-	</header>
+  </header>
+
+  {#if usuario}
+    <div class="card-usuario">
+      <BarraSuperior />
+
+      <div class="usuario">
+        <div class="foto-container">
+          <a href={usuario.perfil_url} target="_blank" rel="noopener">
+            <div
+              class="foto-usuario"
+              style:background-image="url({usuario.avatar_url})"
+            />
+          </a>
+        </div>
+
+        <div class="detalhes-usuario">
+          <div class="info">
+            Nome: <span>{usuario.nome}</span>
+          </div>
+          <div class="info">
+            Usuário: <span>{usuario.login}</span>
+          </div>
+          <div class="info">
+            Seguidores: <span>{usuario.seguidores}</span>
+          </div>
+          <div class="info">
+            Repositórios: <span>{usuario.repositorios_publicos}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  {/if}
 </div>
 
 <style>
-	.app {
+  .app {
     max-height: 100vh;
   }
- 
-  header {    
+
+  header {
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -39,7 +82,7 @@
     position: relative;
     width: 70%;
   }
- 
+
   .input {
     padding: 15px 25px;
     width: calc(100% - 8.75rem);
@@ -49,7 +92,7 @@
     box-shadow: 0px 17px 52px rgba(222, 231, 247, 0.4);
     outline: 0;
   }
- 
+
   .input::placeholder {
     font-family: "Roboto";
     font-style: italic;
@@ -58,7 +101,7 @@
     line-height: 26px;
     color: #6e8cba;
   }
- 
+
   .botao-container {
     position: absolute;
     width: 9.625rem;
@@ -67,7 +110,7 @@
     bottom: 0;
     display: flex;
   }
- 
+
   .botao {
     padding: 15px 24px;
     border-radius: 8px;
@@ -77,16 +120,50 @@
     color: #fff;
     font-size: 22px;
     cursor: pointer;
- 
+
     transition: background-color 0.2s;
- 
+
     display: flex;
     align-items: center;
     gap: 13px;
   }
- 
+
   .botao:hover {
     background: #4590ff;
   }
 
+  .card-usuario {
+    margin-top: 65px;
+  }
+  .usuario {
+    padding: 28px 0;
+    background: rgba(255, 255, 255, 0.5);
+    box-shadow: -12px 37px 45px rgba(133, 127, 201, 0.18);
+    border-radius: 0px 0px 13px 13px;
+    display: flex;
+    justify-content: center;
+  }
+  .foto-container {
+    margin-right: 81px;
+  }
+  .foto-usuario {
+    width: 12.75rem;
+    height: 12.75rem;
+    border: 4.56px solid #2e80fa;
+    border-radius: 50%;
+    background-size: cover;
+  }
+  .detalhes-usuario {
+    margin-right: 55px;
+  }
+  .detalhes-usuario > .info {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 31px;
+    color: #395278;
+  }
+  .detalhes-usuario > .info > span {
+    color: #6781a8;
+    font-weight: normal;
+  }
 </style>
---
+++ src/interfaces/IUsuario.ts
@@ -0,0 +1,8 @@
+export default interface IUsuario {
+  login: string;
+  avatar_url: string;
+  perfil_url: string;
+  nome: string;
+  repositorios_publicos: number;
+  seguidores: number;
+}
